6a9b1165362873f6a74800424fc39a40fc0af531
Jeremy Harris <jeremy@someguyjeremy.com>
2017-08-08 09:08:12 -0500

    Making tests relying on mtime more predictable

--- tests/TestCase/BlogTest.php
+++ tests/TestCase/BlogTest.php
@@ -3,7 +3,6 @@ namespace JeremyHarris\Build\Test\TestCase;
 
 use JeremyHarris\Build\Blog;
 use JeremyHarris\Build\Blog\Post;
-use SplFileObject;
 
 /**
  * Blog test
@@ -66,27 +65,8 @@ class BlogTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetLatest()
     {
-        $moreRecentFile = $this->getMockBuilder(SplFileObject::class)
-            ->setMethods(['getMTime', 'getBasename'])
-            ->setConstructorArgs([
-                TEST_APP . DS . 'views' . DS . '2013' . DS . '05' . DS . 'most-recent.md'
-            ])
-            ->getMock();
-
-        $moreRecentFile
-            ->expects($this->any())
-            ->method('getBasename')
-            ->will($this->returnValue('more-recent'));
-
-        $moreRecentFile
-            ->expects($this->any())
-            ->method('getMTime')
-            ->will($this->returnValue(strtotime('+2 days')));
-
-        $this->Blog->addPost($moreRecentFile);
         $result = $this->Blog->getLatest();
         $this->assertInstanceOf(Post::class, $result);
-        $this->assertSame($moreRecentFile, $result->source());
-        $this->assertSame('more-recent', $result->slug());
+        $this->assertSame('most-recent', $result->slug());
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,15 @@ define('DS', DIRECTORY_SEPARATOR);
 define('TEST_APP', __DIR__ . DS . 'test_site');
 define('FIXTURES', __DIR__ . DS . 'Fixtures');
 define('TEST_BUILD', sys_get_temp_dir());
+
+$mtimes = [
+    '2013' . DS . '05' . DS . 'most-recent.md' => 'Mon, 20 May 2013 15:32:58 +0000',
+    '2013' . DS . '05' . DS . 'my-may-post.md' => 'Sat, 18 May 2013 17:20:00 +0000',
+    '2012' . DS . '01' . DS . 'post-markdown.md' => 'Fri, 20 Jan 2012 01:50:09 +0000',
+    '2012' . DS . '01' . DS . 'post.html' => 'Mon, 16 Jan 2012 15:30:42 +0000',
+    '2012' . DS . '02' . DS . 'another-post.md' => 'Thu, 16 Feb 2012 15:30:42 +0000',
+];
+// Reset modified times for all test_app views so they are predictable in tests
+foreach ($mtimes as $postFilePath => $mtime) {
+    touch(TEST_APP . DS . 'views' . DS . $postFilePath, strtotime($mtime));
+}
